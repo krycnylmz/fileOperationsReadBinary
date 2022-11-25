bc51c77a6758fc25b821a991efb52a671df07ae1
Koray Can Yilmaz <krycnylmz@gmail.com>
2022-11-25 11:56:58 +0300

    base struct

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/PC/Desktop/Lessons/2023/2213-DataStructures/Training/Prelab11
+# Install script for directory: C:/Users/PC/Desktop/Lessons/2023/2213-DataStructures/Training/Prelab11/fileOperationsReadBinary
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/PC/Desktop/Lessons/2023/2213-DataStructures/Training/Prelab11/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/PC/Desktop/Lessons/2023/2213-DataStructures/Training/Prelab11/fileOperationsReadBinary/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
